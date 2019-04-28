17f4f147587b676dafba3275d4d247635f13d333
eric tam <eric.m2hk@gmail.com>
2019-04-28 20:53:28 +0800

    create Register Form Register User with Email and Password Form Validation for Register Form

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -1,9 +1,156 @@
-import React from "react";
+import React, { useState } from "react";
+import { useInput } from "../../customHooks/useInput";
+import firebase from "../../firebase";
+import {
+  Grid,
+  Form,
+  Segment,
+  Button,
+  Header,
+  Message,
+  Icon
+} from "semantic-ui-react";
+
+import { Link } from "react-router-dom";
 
 export default function Register() {
+  const {
+    value: username,
+    bind: bindUsername,
+    reset: resetUsername
+  } = useInput("");
+  const { value: email, bind: bindEmail, reset: resetEmail } = useInput("");
+  const {
+    value: password,
+    bind: bindPassword,
+    reset: resetPassword
+  } = useInput("");
+  const {
+    value: passwordConfirmation,
+    bind: bindPasswordConfirmation,
+    reset: resetPasswordConfirmation
+  } = useInput("");
+  const [errors, setErrors] = useState([]);
+
+  const isFormEmpty = () => {
+    return (
+      !username.length ||
+      !email.length ||
+      !password.length ||
+      !passwordConfirmation.length
+    );
+  };
+
+  const isPasswordValid = () => {
+    if (password.length < 6 || passwordConfirmation.length < 6) {
+      return false;
+    } else if (password !== passwordConfirmation) {
+      return false;
+    } else {
+      return true;
+    }
+  };
+
+  const displayErrors = () => {
+    return errors.map((error, i) => <p key={i}>{error.message}</p>);
+  };
+
+  const isFormValid = () => {
+    let errors = [];
+    let error;
+
+    if (isFormEmpty()) {
+      error = { message: "Fill in all fields" };
+      setErrors(errors.concat(error));
+      return false;
+    } else if (isPasswordValid()) {
+      error = { message: "Password is invalid" };
+      setErrors(errors.concat(error));
+      return false;
+    } else {
+      return true;
+    }
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    if (isFormValid()) {
+      firebase
+        .auth()
+        .createUserWithEmailAndPassword(email, password)
+        .then(createdUser => {
+          console.log(createdUser);
+          resetUsername();
+          resetEmail();
+          resetPassword();
+          resetPasswordConfirmation();
+        })
+        .catch(err => console.log(err));
+    }
+  };
+
   return (
-    <div>
-      <p>Register</p>
-    </div>
+    <Grid textAlign="center" verticalAlign="middle" className="app">
+      <Grid.Column style={{ maxWidth: 450 }}>
+        <Header as="h2" icon color="orange" textAlign="center">
+          <Icon name="puzzle piece" color="orange" />
+          Register for DevChat
+        </Header>
+        <Form onSubmit={handleSubmit} size="large">
+          <Segment stacked>
+            <Form.Input
+              fluid
+              name="username"
+              icon="user"
+              iconPosition="left"
+              placeholder="Username"
+              {...bindUsername}
+              type="text"
+            />
+            <Form.Input
+              fluid
+              name="email"
+              icon="mail"
+              iconPosition="left"
+              placeholder="Email Address"
+              {...bindEmail}
+              type="email"
+            />
+            <Form.Input
+              fluid
+              name="password"
+              icon="lock"
+              iconPosition="left"
+              placeholder="Password"
+              {...bindPassword}
+              type="password"
+            />
+            <Form.Input
+              fluid
+              name="passwordConfirmation"
+              icon="repeat"
+              iconPosition="left"
+              placeholder="Password Confirm"
+              {...bindPasswordConfirmation}
+              type="password"
+            />
+
+            <Button color="orange" fluid size="large">
+              Submit
+            </Button>
+          </Segment>
+        </Form>
+        {errors.length > 0 && (
+          <Message error>
+            <h3>Error</h3>
+            {displayErrors()}
+          </Message>
+        )}
+        <Message>
+          Already a user? <Link to="/login">Login</Link>
+        </Message>
+      </Grid.Column>
+    </Grid>
   );
 }
