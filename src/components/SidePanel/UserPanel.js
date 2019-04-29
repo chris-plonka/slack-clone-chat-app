eda73b5089103c37a08b303749aebc7a10db7297
eric tam <eric.m2hk@gmail.com>
2019-04-29 19:24:50 +0800

    display the username from state

--- src/components/SidePanel/UserPanel.js
+++ src/components/SidePanel/UserPanel.js
@@ -1,36 +1,39 @@
-import React from "react";
+import React, { useContext } from "react";
 import firebase from "../../firebase";
+import Store from "../../Store";
 import { Grid, Header, Icon, Dropdown } from "semantic-ui-react";
 
-const handleSignout = () => {
-  firebase
-    .auth()
-    .signOut()
-    .then(() => console.log("signout"))
-    .catch(err => console.log(err));
-};
-
-const dropdownOptions = () => [
-  {
-    key: "user",
-    text: (
-      <span>
-        Signed in as <strong>User</strong>
-      </span>
-    ),
-    disabled: true
-  },
-  {
-    key: "avatar",
-    text: <span>Change Avatar</span>
-  },
-  {
-    key: "signout",
-    text: <span onClick={handleSignout}>Sign Out</span>
-  }
-];
-
 export default function UserPanel() {
+  const { state, dispatch } = useContext(Store);
+
+  const handleSignout = () => {
+    firebase
+      .auth()
+      .signOut()
+      .then(() => console.log("signout"))
+      .catch(err => console.log(err));
+  };
+
+  const dropdownOptions = () => [
+    {
+      key: "user",
+      text: (
+        <span>
+          Signed in as <strong>{state.user.currentUser.displayName}</strong>
+        </span>
+      ),
+      disabled: true
+    },
+    {
+      key: "avatar",
+      text: <span>Change Avatar</span>
+    },
+    {
+      key: "signout",
+      text: <span onClick={handleSignout}>Sign Out</span>
+    }
+  ];
+
   return (
     <Grid style={{ background: "#4c3c4c" }}>
       <Grid.Column>
@@ -43,7 +46,10 @@ export default function UserPanel() {
         </Grid.Row>
         {/* User Dropdown */}
         <Header style={{ padding: "0.25em" }} as="h4" inverted>
-          <Dropdown trigger={<span>User</span>} options={dropdownOptions()} />
+          <Dropdown
+            trigger={<span>{state.user.currentUser.displayName}</span>}
+            options={dropdownOptions()}
+          />
         </Header>
       </Grid.Column>
     </Grid>
