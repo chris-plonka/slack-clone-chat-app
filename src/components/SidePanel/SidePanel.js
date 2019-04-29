7f8b79c8090aa10d82c100e0ac4c6b2d7fb3d991
eric tam <eric.m2hk@gmail.com>
2019-04-29 18:38:40 +0800

    Scaffold App components And add App Header / UserPanel Dropdown

---
+++ src/components/SidePanel/SidePanel.js
@@ -0,0 +1,17 @@
+import React from "react";
+import UserPanel from "./UserPanel";
+import { Menu } from "semantic-ui-react";
+
+export default function SidePanel() {
+  return (
+    <Menu
+      size="large"
+      inverted
+      fixed="left"
+      vertical
+      style={{ background: "#4c3c4c", fontSize: "1.2rem" }}
+    >
+      <UserPanel />
+    </Menu>
+  );
+}
