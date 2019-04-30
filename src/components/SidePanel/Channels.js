dc1fdfa3c3debce5c837a98efaa3f74d8f5de083
eric tam <eric.m2hk@gmail.com>
2019-04-30 10:49:53 +0800

    create channel collection add channel to database

--- src/components/SidePanel/Channels.js
+++ src/components/SidePanel/Channels.js
@@ -1,8 +1,11 @@
-import React, { useState, Fragment } from "react";
+import React, { useState, useContext, Fragment } from "react";
 import { useInput } from "../../customHooks/useInput";
+import firebase from "../../firebase";
+import Store from "../../Store";
 import { Menu, Icon, Modal, Form, Input, Button } from "semantic-ui-react";
 
 export default function Channels() {
+  const { state, dispatch } = useContext(Store);
   const {
     value: channelName,
     bind: bindChannelName,
@@ -15,6 +18,11 @@ export default function Channels() {
   } = useInput("");
   const [channels, setChannels] = useState([]);
   const [modal, setModal] = useState(false);
+  const [errors, setErrors] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [channelsRef, setChannelsRef] = useState(
+    firebase.database().ref("channels")
+  );
 
   const closeModal = () => {
     setModal(false);
@@ -26,6 +34,59 @@ export default function Channels() {
     setModal(true);
   };
 
+  const isFormValid = () => {
+    let errors = [];
+    let error;
+
+    if (channelName.length <= 0) {
+      error = { message: "Fill in the Name of Channel" };
+      errors.push(error);
+    }
+    if (channelDetails.length <= 0) {
+      error = { message: "Fill in the About the Channel" };
+      errors.push(error);
+    }
+    if (errors.length > 0) {
+      setErrors(errors);
+      return false;
+    } else {
+      return true;
+    }
+  };
+
+  const addChannel = () => {
+    const key = channelsRef.push().key;
+    const newChannel = {
+      id: key,
+      name: channelName,
+      details: channelDetails,
+      createdBy: {
+        name: state.user.currentUser.displayName,
+        avatar: state.user.currentUser.photoURL
+      }
+    };
+
+    channelsRef
+      .child(key)
+      .update(newChannel)
+      .then(() => {
+        closeModal();
+        console.log("channel added");
+      })
+      .catch(err => {
+        console.error(err);
+        setLoading(false);
+        setErrors([err]);
+      });
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (isFormValid()) {
+      addChannel();
+    }
+  };
+
   return (
     <Fragment>
       <Menu.Menu style={{ paddingBottom: "2em" }}>
@@ -33,7 +94,8 @@ export default function Channels() {
           <span>
             <Icon name="exchange" /> CHANNELS
           </span>
-          &nbsp; ({channels.length}) <Icon name="add" onClick={openModal} />
+          &nbsp; ({channels.length}){" "}
+          <Icon link name="add" onClick={openModal} />
         </Menu.Item>
         {/* channels */}
       </Menu.Menu>
@@ -42,12 +104,13 @@ export default function Channels() {
       <Modal basic open={modal} onClose={closeModal}>
         <Modal.Header>Add a Channel</Modal.Header>
         <Modal.Content>
-          <Form>
+          <Form onSubmit={handleSubmit}>
             <Form.Field>
               <Input
                 fluid
                 label="Name of Channel"
                 name="channelName"
+                autoFocus
                 {...bindChannelName}
               />
             </Form.Field>
@@ -63,7 +126,7 @@ export default function Channels() {
         </Modal.Content>
 
         <Modal.Actions>
-          <Button color="green" inverted>
+          <Button color="green" inverted onClick={handleSubmit}>
             <Icon name="checkmark" /> Add
           </Button>
           <Button color="red" inverted onClick={closeModal}>
